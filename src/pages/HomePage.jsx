c033c3a13f16ca65ad545e8ad62be84e27edd35d
unknown <jonathannzete@gmail.com>
2022-01-23 22:10:49 +0100

    home tabs styled

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,25 +9,25 @@ const HomePage = () => {
   }
   return (
     <div className="home flex-2">
-      <Card className="home__tabs">
+      <Card className="home__tabs shadow-sm">
         <div className="row">
           <div
             className={`feeds col ${tabIndex === 0 ? "active" : ""}`}
             onClick={() => getIndex(0)}
           >
-            <p className=" text-muted m-0 text-center py-3">FEEDS</p>
+            <p className=" text-muted m-0 text-center py-2">FEEDS</p>
           </div>
           <div
             className={`people col ${tabIndex === 1 ? "active" : ""}`}
             onClick={() => getIndex(1)}
           >
-            <p className=" text-muted m-0 text-center py-3">PEOPLE</p>
+            <p className=" text-muted m-0 text-center py-2">PEOPLE</p>
           </div>
           <div
             className={`trending col ${tabIndex === 2 ? "active" : ""}`}
             onClick={() => getIndex(2)}
           >
-            <p className=" text-muted m-0 text-center py-3">TRENDING</p>
+            <p className=" text-muted m-0 text-center py-2">TRENDING</p>
           </div>
         </div>
       </Card>
