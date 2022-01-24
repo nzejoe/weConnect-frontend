b054a72a7b79844cfccfbab672d588353ca345ed
unknown <jonathannzete@gmail.com>
2022-01-24 12:43:20 +0100

    nav icons added

--- src/components/CusNav.jsx
+++ src/components/CusNav.jsx
@@ -1,34 +1,87 @@
-import React from 'react';
-import { Container, Row, Col } from "react-bootstrap";
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { Nav, NavItem, NavLink } from "react-bootstrap";
+import { NavLink as RLink } from "react-router-dom";
+
+// react icons
+import {
+  MdHome,
+  MdAccountCircle,
+  MdExplore,
+  MdOutlineTranslate,
+  MdOutlineLogout,
+  MdOutlineWeb,
+  MdOutlineLocalFireDepartment,
+} from "react-icons/md";
 
 const CusNav = ({ show, setShow }) => {
   return (
     <aside className={`cus__col cus__nav ${show ? "show" : "hide"}`}>
       <div className="backdrop" onClick={() => setShow(false)}></div>
-      <ul className="content navbar-nav justify-content-center flex-grow-1">
-        <li className="nav-item mb-4">
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li className="nav-item mb-4">
-          <NavLink to="/profile/2345/">Profile</NavLink>
-        </li>
-        <li className="nav-item mb-4">
-          <NavLink to="/profile/2345/">Explore</NavLink>
-        </li>
-        <li className="nav-item mb-4">
-          <NavLink to="/profile/2345/">Language</NavLink>
-        </li>
-        <li className="nav-item mb-4">
-          <NavLink to="/profile/2345/">Logout</NavLink>
-        </li>
-        <li className="nav-item mb-4">
-          <NavLink to="/">Pages</NavLink>
-        </li>
-        <li className="nav-item mb-4">
-          <NavLink to="/">Trending</NavLink>
-        </li>
-      </ul>
+      <Nav className="content navbar-nav justify-content-center flex-grow-1">
+        <NavItem className="mb-4">
+          <NavLink
+            as={RLink}
+            to="/"
+            className="text-dark text-upper text-muted"
+          >
+            <MdHome /> Home
+          </NavLink>
+        </NavItem>
+        <NavItem className="mb-4">
+          <NavLink
+            as={RLink}
+            to="/profile/2345/"
+            className="text-dark text-upper text-muted"
+          >
+            <MdAccountCircle /> Profile
+          </NavLink>
+        </NavItem>
+        <NavItem className="mb-4">
+          <NavLink
+            as={RLink}
+            to="/profile/2345/"
+            className="text-dark text-upper text-muted"
+          >
+           <MdExplore/> Explore
+          </NavLink>
+        </NavItem>
+        <NavItem className="mb-4">
+          <NavLink
+            as={RLink}
+            to="/profile/2345/"
+            className="text-dark text-upper text-muted"
+          >
+            <MdOutlineTranslate/> Language
+          </NavLink>
+        </NavItem>
+        <NavItem className="mb-4">
+          <NavLink
+            as={RLink}
+            to="/profile/2345/"
+            className="text-dark text-upper text-muted"
+          >
+            <MdOutlineLogout/> Logout
+          </NavLink>
+        </NavItem>
+        <NavItem className="mb-4">
+          <NavLink
+            as={RLink}
+            to="/"
+            className="text-dark text-upper text-muted"
+          >
+            <MdOutlineWeb/> Pages
+          </NavLink>
+        </NavItem>
+        <NavItem className="mb-4">
+          <NavLink
+            as={RLink}
+            to="/"
+            className="text-dark text-upper text-muted"
+          >
+            <MdOutlineLocalFireDepartment/> Trending
+          </NavLink>
+        </NavItem>
+      </Nav>
     </aside>
   );
 };
