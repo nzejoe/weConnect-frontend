410d5b659f6b9571785e1426da301cfee972ba23
unknown <jonathannzete@gmail.com>
2022-01-30 10:01:09 +0100

    password reset form styled

--- src/components/UnauthenticatedUser.jsx
+++ src/components/UnauthenticatedUser.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import { MdPeople } from "react-icons/md";
 
-const UnauthenticatedUser = ({ children }) => {
+const UnauthenticatedUser = ({ children, className }) => {
   return (
-    <Container className="unauthenticated-user vh-100 pt-5">
+    <Container className={`unauthenticated-user vh-100 pt-5 ${className}`}>
       <Row xs={1} md={2}>
         <Col className="d-flex justify-content-center mb-5">
           <div className="brand-container d-flex align-items-center">
@@ -12,9 +12,7 @@ const UnauthenticatedUser = ({ children }) => {
             weConnect
           </div>
         </Col>
-        <Col className="d-flex justify-content-center">
-          {children}
-        </Col>
+        <Col className="d-flex justify-content-center">{children}</Col>
       </Row>
     </Container>
   );
--- src/pages/PasswordResetPage.jsx
+++ src/pages/PasswordResetPage.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import { Form, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
+import { Form, Card, Button } from 'react-bootstrap'
 import { UnauthenticatedUser } from '../components';
 
 const PasswordResetPage = () => {
-  return <UnauthenticatedUser>
-      <Card className='password-reset-form p-3'>
-          <Form>
-              <Form.Group>
-                  <Form.Control placeholder='Email'/>
-              </Form.Group>
-          </Form>
+  return (
+    <UnauthenticatedUser className="password-reset-page">
+      <Card className="password-reset-form p-3">
+        <h5 className="py-3">Password reset</h5>
+        <p class="form__text">
+          Please enter a valid email address linked to your account.
+        </p>
+        <Form className="mb-4">
+          <Form.Group className="mb-3">
+            <Form.Control placeholder="Email" size='lg'/>
+          </Form.Group>
+          <Form.Group>
+            <Button type="submit" className="w-100">
+              Send request
+            </Button>
+          </Form.Group>
+        </Form>
+        <hr />
+        <div className='text-muted'>
+          Have passord?{" "}
+          <Link to="/account/login/" className="btn btn-link">
+            Log in
+          </Link>
+        </div>
       </Card>
-  </UnauthenticatedUser>;
+    </UnauthenticatedUser>
+  );
 };
 
 export default PasswordResetPage;
