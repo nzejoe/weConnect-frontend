30dd7f0593d8ac9100532ef105dc9dad7466f887
unknown <jonathannzete@gmail.com>
2022-01-31 20:23:40 +0100

    user form login validation added

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,41 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import {
-  Form,
-  FormGroup,
-  FormControl,
-  Button,
-  InputGroup,
-} from "react-bootstrap";
+import { Form, FormGroup, Button } from "react-bootstrap";
 import { BsEyeSlash, BsEye } from "react-icons/bs";
+
+import { Input } from ".";
+import useInput from "../hooks/use-input";
 
 const LoginForm = () => {
   const [show, setShow] = useState(false);
+  const [formHasError, setFormHasError] = useState(false);
 
   const toggleShow = () => {
     setShow(!show);
   };
 
+  //   EMAIL
+  function validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  const {
+    value: email,
+    isValid: isEmailValid,
+    hasError: emailHasError,
+    onChange: onEmailChange,
+    onBlur: onEmailBlur,
+  } = useInput(validateEmail);
+
+  //   PASSWORD
+  const validatePassword = (password) => {
+    return password.length !== 0;
+  };
+  const {
+    value: password,
+    // isValid: isPasswordValid,
+    hasError: passwordHasError,
+    onChange: onPasswordChange,
+    onBlur: onPasswordBlur,
+  } = useInput(validatePassword);
+
+  useEffect(() => {
+    setFormHasError(emailHasError || passwordHasError);
+  }, [emailHasError, passwordHasError]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!formHasError) {
+      const data = {
+        username: email,
+        password: password,
+      };
+      console.log(data);
+    }
+  };
+
+  console.log(formHasError);
+
   return (
     <div>
-      <Form>
-        <FormGroup className="mb-3">
-          <FormControl
-            type="email"
-            placeholder="Email"
-            className="form-control-lg"
-          />
-        </FormGroup>
-        <InputGroup className="mb-3">
-          <FormControl
-            type={`${!show ? 'password' : 'text'}`}
-            placeholder="Password"
-            className="form-control-lg"
-          />
-          <InputGroup.Text onClick={toggleShow} className="bg-white">
-            {show ? <BsEye className="clickable"/> : <BsEyeSlash className="clickable"/> }
-          </InputGroup.Text>
-        </InputGroup>
+      <Form onSubmit={submitHandler}>
+        <Input
+          type="email"
+          placeholder="Email"
+          value={email}
+          isValid={isEmailValid}
+          hasError={emailHasError}
+          onChange={onEmailChange}
+          onBlur={onEmailBlur}
+          errorMessage={
+            !email ? "Please enter your email address" : "Inavlid email address"
+          }
+        />
+        <Input
+          type={`${!show ? "password" : "text"}`}
+          placeholder="Password"
+          value={password}
+          onChange={onPasswordChange}
+          onBlur={onPasswordBlur}
+          hasError={passwordHasError}
+          errorMessage={"Please provide your password"}
+          icon={
+            show ? (
+              <BsEye className="clickable" onClick={toggleShow} />
+            ) : (
+              <BsEyeSlash className="clickable" onClick={toggleShow} />
+            )
+          }
+        />
         <FormGroup className="mb-3">
           <Button type="submit" variant="primary" className="w-100 btn-lg">
             Log in
--- src/pages/PrivateRoute.jsx
+++ src/pages/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 const PublicRoute = ({ children }) => {
-    const isAthenticated = true;
+    const isAthenticated = false;
   return isAthenticated ? children : <Navigate replace to='/account/login/' />;
 };
 
