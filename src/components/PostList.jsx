ab3285935bd58e6a055169d692b794bdd741e103
unknown <jonathannzete@gmail.com>
2022-02-03 09:34:01 +0100

    user post now displayed on frontend

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import {
   Card,
   Image,
@@ -9,109 +9,82 @@ import {
   MdOutlineThumbUp,
 } from "react-icons/md";
 
+import { baseURL, debug } from "../utils";
+
 import { CommentList } from ".";
 
+// context
+import { PostContext } from "../store/post-context";
+
 const PostList = () => {
+  const { authUserPosts } = useContext(PostContext);
+
+  console.log(authUserPosts)
   
   return (
     <div className="post-list my-4">
-      <div className="post my-2">
-        <Card>
-          <div className="post-header mb-3 p-3 d-flex justify-content-between align-items-center">
-            <div className="post-author d-flex">
-              <Image
-                fluid
-                roundedCircle
-                width={60}
-                src="/img/profile/profile-4.jpg"
-              />
-              <div className="author-info ms-2">
-                <Card.Title className="m-0">Maya Jonathan</Card.Title>
-                <Card.Text className="text-muted m-0 username">@maya</Card.Text>
+      { authUserPosts && authUserPosts.map(post=>{
+        const fullName = `${post.author.first_name} ${post.author.last_name}`;
+        return (
+          <div className="post my-3" key={post.id}>
+            <Card>
+              <div className="post-header mb-3 p-3 d-flex justify-content-between align-items-center">
+                <div className="post-author d-flex">
+                  <Image
+                    fluid
+                    roundedCircle
+                    width={60}
+                    src={
+                      debug
+                        ? `${baseURL + post.author.avatar}`
+                        : `${post.author.avatar}`
+                    }
+                  />
+                  <div className="author-info ms-2">
+                    <Card.Title className="m-0">{fullName}</Card.Title>
+                    <Card.Text className="text-muted m-0 username">
+                      @{post.author.username}
+                    </Card.Text>
+                  </div>
+                </div>
+                <div className="post-date">
+                  <p className="text-muted date d-flex align-items-center">
+                    {post.created}{" "}
+                    <MdMoreVert className="option-icon ms-2 p-1" />
+                  </p>
+                </div>
               </div>
-            </div>
-            <div className="post-date">
-              <p className="text-muted date d-flex align-items-center">
-                23 Dec <MdMoreVert className="option-icon ms-2 p-1" />
-              </p>
-            </div>
-          </div>
-          <div className="post-body">
-            <p className="post-text text-muted px-3">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
-              tempore ratione perferendis iure quisquam consectetur. Est magni
-              ut possimus rem! Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Enim, unde!
-            </p>
-            <Card.Img
-              className="post-img"
-              src="/img/post/post-3.jpg"
-            ></Card.Img>
-          </div>
-          <div className="post-actions py-2">
-            <div className="count d-flex text-muted mb-1 px-4">
-              <div className="like clickable me-5">
-                <MdOutlineThumbUp className="count-icon" /> 20k
+              <div className="post-body">
+                <p className="post-text text-muted px-3">{post.text}</p>
+                {post.image && (
+                  <Card.Img
+                    className="post-img"
+                    src={
+                      debug
+                        ? `${baseURL + post.image}`
+                        : `${post.image}`
+                    }
+                  ></Card.Img>
+                )}
               </div>
-              <div className="comment-count clickable">
-                <MdChatBubbleOutline className="count-icon" /> 10k
-              </div>
-            </div>
+              <div className="post-actions py-2">
+                <div className="count d-flex text-muted mb-1 px-4">
+                  <div className="like clickable me-5">
+                    <MdOutlineThumbUp className="count-icon" /> 20k
+                  </div>
+                  <div className="comment-count clickable">
+                    <MdChatBubbleOutline className="count-icon" /> 10k
+                  </div>
+                </div>
 
-            {/* comment list */}
-            <CommentList />
-          </div>
-        </Card>
-      </div>
-
-      <div className="post my-3">
-        <Card>
-          <div className="post-header mb-3 p-3 d-flex justify-content-between align-items-center">
-            <div className="post-author d-flex">
-              <Image
-                fluid
-                roundedCircle
-                width={60}
-                src="/img/profile/profile-5.jpg"
-              />
-              <div className="author-info ms-2">
-                <Card.Title className="m-0">Sara Williams</Card.Title>
-                <Card.Text className="text-muted m-0 username">@sara</Card.Text>
+                {/* comment list */}
+                <CommentList />
               </div>
-            </div>
-            <div className="post-date">
-              <p className="text-muted date d-flex align-items-center">
-                21 Dec <MdMoreVert className="option-icon ms-2 p-1" />
-              </p>
-            </div>
+            </Card>
           </div>
-          <div className="post-body">
-            <p className="post-text text-muted px-3">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
-              tempore ratione perferendis iure quisquam consectetur. Est magni
-              ut possimus rem! Lorem ipsum dolor sit amet consectetur
-              adipisicing elit. Enim, unde!
-            </p>
-            <Card.Img
-              className="post-img"
-              src="/img/post/post-2.jpg"
-            ></Card.Img>
-          </div>
-          <div className="post-actions py-2">
-            <div className="count d-flex text-muted mb-1 px-4">
-              <div className="like clickable me-5">
-                <MdOutlineThumbUp className="count-icon" /> 10k
-              </div>
-              <div className="comment-count clickable">
-                <MdChatBubbleOutline className="count-icon" /> 3k
-              </div>
-            </div>
-
-            {/* comment list */}
-            <CommentList />
-          </div>
-        </Card>
-      </div>
+        );
+      })
+      }
     </div>
   );
 };
