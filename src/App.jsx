89f95645b1c881d49559008de4f1c7fe10887eaa
unknown <jonathannzete@gmail.com>
2022-02-07 13:59:12 +0100

    comment context removed from App component

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,11 @@ import {
   RegisterPage,
   PasswordResetPage,
   PasswordResetComplete,
-  PrivateRoute
+  PrivateRoute,
 } from "./pages";
 
 import AuthUserProvider from "./store/auth-user-context";
 import PostProvider from "./store/post-context";
-import CommentProvider from "./store/comment-context";
 
 // utils
 import { baseURL } from "./utils";
@@ -32,75 +31,70 @@ function App() {
   return (
     <AuthUserProvider>
       <PostProvider>
-        <CommentProvider>
-          <Router>
-            <Routes>
-              {/* PRIVATE ROUTES */}
-              <Route
-                path="/"
-                element={
-                  <PrivateRoute>
-                    <HomePage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/profile/:id/"
-                element={
-                  <PrivateRoute>
-                    <ProfilePage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/explore/"
-                element={
-                  <PrivateRoute>
-                    <ExplorePage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/trending/"
-                element={
-                  <PrivateRoute>
-                    <TrendingPage />
-                  </PrivateRoute>
-                }
-              />
-              <Route
-                path="/language/"
-                element={
-                  <PrivateRoute>
-                    <LanguagePage />
-                  </PrivateRoute>
-                }
-              />
+        <Router>
+          <Routes>
+            {/* PRIVATE ROUTES */}
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile/:id/"
+              element={
+                <PrivateRoute>
+                  <ProfilePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/explore/"
+              element={
+                <PrivateRoute>
+                  <ExplorePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/trending/"
+              element={
+                <PrivateRoute>
+                  <TrendingPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/language/"
+              element={
+                <PrivateRoute>
+                  <LanguagePage />
+                </PrivateRoute>
+              }
+            />
 
-              {/* PUBLIC ROUTES */}
-              <Route
-                path="/account/login/"
-                element={<PublicRoute children={<LoginPage />} />}
-              />
-              <Route
-                path="/account/register/"
-                element={<PublicRoute children={<RegisterPage />} />}
-              />
-              <Route
-                path="/account/password_reset/"
-                element={<PublicRoute children={<PasswordResetPage />} />}
-              />
-              <Route
-                path="/account/password_reset_complete/"
-                element={<PublicRoute children={<PasswordResetComplete />} />}
-              />
-              <Route
-                path="*"
-                element={<PublicRoute children={<NotFound />} />}
-              />
-            </Routes>
-          </Router>
-        </CommentProvider>
+            {/* PUBLIC ROUTES */}
+            <Route
+              path="/account/login/"
+              element={<PublicRoute children={<LoginPage />} />}
+            />
+            <Route
+              path="/account/register/"
+              element={<PublicRoute children={<RegisterPage />} />}
+            />
+            <Route
+              path="/account/password_reset/"
+              element={<PublicRoute children={<PasswordResetPage />} />}
+            />
+            <Route
+              path="/account/password_reset_complete/"
+              element={<PublicRoute children={<PasswordResetComplete />} />}
+            />
+            <Route path="*" element={<PublicRoute children={<NotFound />} />} />
+          </Routes>
+        </Router>
       </PostProvider>
     </AuthUserProvider>
   );
