907ff8bc38b46ff4db8a49d2c5c89ca00d0363ef
unknown <jonathannzete@gmail.com>
2022-02-07 22:21:34 +0100

    sending password reset request to backend

--- src/pages/PasswordResetPage.jsx
+++ src/pages/PasswordResetPage.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Form, Card, Button } from "react-bootstrap";
+import { Form, Card, Button, Alert, Row, Col } from "react-bootstrap";
+import axios from "axios";
+
 import { UnauthenticatedUser } from "../components";
 
 import { Input } from "../components";
@@ -9,6 +11,8 @@ import useInput from "../hooks/use-input";
 const PasswordResetPage = () => {
   const [formValid, setFormValid] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [resetDone, setResetDone] = useState(false);
 
   //   EMAIL
   function validateEmail(email) {
@@ -28,52 +32,90 @@ const PasswordResetPage = () => {
     }
   }, [isValid]);
 
-  const submitHandler = (e) => {
+  const submitHandler = async(e) => {
     e.preventDefault();
-    const data = {email: value}
-    console.log(data)
+    if(formValid){
+      const data = { email: value };
+      setIsLoading(true)
+      try {
+        const response = await axios({
+          url: '/users/password_reset/',
+          method: 'POST',
+          headers:{
+            'Content-type': 'application/json'
+          },
+          data: data,
+        })
+        if(response.status === 200){
+          setIsLoading(false);
+          setResetDone(true);
+          console.log(response.data)
+        }
+      } catch (error) {
+        const err = {...error};
+        const errorData = err.response.data;
+        if (errorData.email) setError({ message: errorData.email[0] });
+        console.log(errorData)
+        setIsLoading(false);
+      }
+    }
+    
   }
 
   return (
     <UnauthenticatedUser className="password-reset-page">
-      <Card className="password-reset-form p-3">
-        <h5 className="py-3">Password reset</h5>
-        <p className="form__text mb-0">
-          Please enter a valid email address linked to your account.
-        </p>
-        <Form className="mb-4" onSubmit={submitHandler}>
-          <Input
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={value}
-            onChange={onChange}
-            onBlur={onBlur}
-            hasError={hasError}
-            errorMessage={
-              value
-                ? "Invalid email address."
-                : "Your email address is required."
-            }
-          />
-          <Form.Group>
-            <Button
-              type="submit"
-              className="w-100"
-              disabled={!formValid || isLoading}
-            >
-              Send request
-            </Button>
-          </Form.Group>
-        </Form>
-        <hr />
-        <div className="text-muted">
-          Have passord?{" "}
-          <Link to="/account/login/" className="btn btn-link">
-            Log in
-          </Link>
-        </div>
-      </Card>
+      {!resetDone ? (
+        <Card className="password-reset-form p-3">
+          <h5 className="py-3">Password reset</h5>
+          <p className="form__text mb-0">
+            Please enter a valid email address linked to your account.
+          </p>
+          <Form className="mb-4" onSubmit={submitHandler}>
+            <Input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={value}
+              onChange={onChange}
+              onBlur={onBlur}
+              hasError={hasError}
+              errorMessage={
+                value
+                  ? "Invalid email address."
+                  : "Your email address is required."
+              }
+            />
+            <Form.Group>
+              {error && <Alert variant="danger">{error.message}</Alert>}
+              <Button
+                type="submit"
+                className="w-100"
+                disabled={!formValid || isLoading}
+              >
+                Send request
+              </Button>
+            </Form.Group>
+          </Form>
+          <hr />
+          <div className="text-muted">
+            Have passord?{" "}
+            <Link to="/account/login/" className="btn btn-link">
+              Log in
+            </Link>
+          </div>
+        </Card>
+      ) : (
+        <Row xs={1} md={2}>
+          <Col md="auto">
+            <Alert variant="success">
+              <h5>Password reset done!</h5>
+              <p>
+                A link on how to set a new password has been sent to {value}.
+              </p>
+            </Alert>
+          </Col>
+        </Row>
+      )}
     </UnauthenticatedUser>
   );
 };
