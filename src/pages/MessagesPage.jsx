405c3b0379d99317acf9647ca6c4b05a0df34a40
unknown <jonathannzete@gmail.com>
2022-02-13 10:26:41 +0100

    temp chatMessages file created and rendered at messages page

--- src/pages/MessagesPage.jsx
+++ src/pages/MessagesPage.jsx
@@ -1,19 +1,44 @@
-import React from "react";
-import { Card } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Card, Image } from "react-bootstrap";
 import { BsEnvelope } from "react-icons/bs";
-
+import { AuthUserContext } from "../store/auth-user-context";
+// temp
+import { chatMessages } from "../utils/temp";
 import { Base } from "../components";
 
 const MessagesPage = () => {
+  const { user } = useContext(AuthUserContext);
   return (
     <Base>
-      <Card className={`messages`}>
+      <Card className={`messages p-0`}>
         <Card.Header className="d-flex justify-content-between bg-primary text-white">
           <Card.Title className="m-0">Messages</Card.Title>
           <BsEnvelope />
         </Card.Header>
         <Card.Body>
-          <p>hi everyone</p>
+            {chatMessages.map(thread=>{
+                const otherUser = user && thread.messages.filter(chat=>{
+                    return chat.user.username !== user.username
+                })
+                const otherUserLast = otherUser[otherUser.length - 1];
+                return (
+                  <Card.Body
+                    key={thread.id}
+                    className="thread d-flex clickable border-bottom"
+                  >
+                    <Image
+                      src={otherUserLast.user.avatar}
+                      width={50}
+                      height={50}
+                      roundedCircle
+                    />
+                    <div className="ms-4">
+                      <h6 className="mb-0">{otherUserLast.user.username}</h6>
+                      <p className="text-muted">{otherUserLast.message}</p>
+                    </div>
+                  </Card.Body>
+                );
+            })}
         </Card.Body>
       </Card>
     </Base>
---
+++ src/utils/temp.js
@@ -0,0 +1,147 @@
+export const chatMessages = [
+  {
+    id: '1',
+    messages: [
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "nzejoe", avatar: "/img/profile/profile-2.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "how are you?",
+      },
+      {
+        user: { username: "nzejoe", avatar: "/img/profile/profile-2.jpg" },
+        message: "i'm good and you?",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "not much",
+      },
+      {
+        user: { username: "nzejoe", avatar: "/img/profile/profile-2.jpg" },
+        message: "not much! how?",
+      },
+    ],
+  },
+  {
+    id: '2',
+    messages: [
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "devin", avatar: "/img/profile/profile-3.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "how are you?",
+      },
+      {
+        user: { username: "devin", avatar: "/img/profile/profile-3.jpg" },
+        message: "i'm good and you?",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "not much",
+      },
+      {
+        user: { username: "devin", avatar: "/img/profile/profile-3.jpg" },
+        message: "hope you are ok?",
+      },
+    ],
+  },
+  {
+    id: '3',
+    messages: [
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "jane", avatar: "/img/profile/profile-4.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "how are you?",
+      },
+      {
+        user: { username: "jane", avatar: "/img/profile/profile-4.jpg" },
+        message: "i'm good and you?",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "not much",
+      },
+      {
+        user: { username: "jane", avatar: "/img/profile/profile-4.jpg" },
+        message: "i'm coming over",
+      },
+    ],
+  },
+  {
+    id: '4',
+    messages: [
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "nzejoe", avatar: "/img/profile/profile-2.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "how are you?",
+      },
+      {
+        user: { username: "nzejoe", avatar: "/img/profile/profile-2.jpg" },
+        message: "i'm good and you?",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "not much",
+      },
+      {
+        user: { username: "nzejoe", avatar: "/img/profile/profile-2.jpg" },
+        message: "not much! how?",
+      },
+    ],
+  },
+  {
+    id: '5',
+    messages: [
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "meloney", avatar: "/img/profile/profile-5.jpg" },
+        message: "hi",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "how are you?",
+      },
+      {
+        user: { username: "meloney", avatar: "/img/profile/profile-5.jpg" },
+        message: "i'm good and you?",
+      },
+      {
+        user: { username: "maya", avatar: "/img/profile/profile-1.jpg" },
+        message: "not much",
+      },
+      {
+        user: { username: "meloney", avatar: "/img/profile/profile-5.jpg" },
+        message: "we have test tommorow",
+      },
+    ],
+  },
+];
